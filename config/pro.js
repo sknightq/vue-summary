e152b15e45852dd29328a5b5c8d697ce41fb7c7b
Sknight Qian <sknight127@sina.com>
2020-12-16 14:51:34 +0800

    Update pro.js

--- config/pro.js
+++ config/pro.js
@@ -2,7 +2,28 @@ const merge = require('webpack-merge')
 const baseConfig = require('./base')
 
 const proConfig = merge(baseConfig, {
-  configureWebpack: {}
+  configureWebpack: {
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  }
 })
 
 module.exports = proConfig
