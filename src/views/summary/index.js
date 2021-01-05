8977a11272bd451a458510e41a39d20c7dac7b00
Sknight Qian <sknight127@sina.com>
2021-01-05 14:48:07 +0800

    Update index.js

--- src/views/summary/index.js
+++ src/views/summary/index.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './index.vue'
+import store from '@/store/index.js'
 import '@/assets/styles/base.scss'
-createApp(App).mount('#app')
+import '@/assets/styles/animate.scss'
+var instance = createApp(App)
+  .use(store)
+  .mount('#container')
+window.getProgress = function(...args) {
+  return App.methods.getProgress.apply(instance, args)
+}
+window.completeProgress = function() {
+  App.methods.completeProgress.apply(instance)
+}
+
+window.showScreens = function() {
+  App.methods.showScreens.apply(instance)
+}
