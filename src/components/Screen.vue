04e147e93035eed2b6913c77d35c9187ec7d42eb
Sknight Qian <sknight127@sina.com>
2021-01-15 16:58:16 +0800

    Update Screen.vue

--- src/components/Screen.vue
+++ src/components/Screen.vue
@@ -1,11 +1,12 @@
 <template>
-  <section class="screen">
-    <component :is="componentId"></component>
+  <section class="wrapper" :style="screenStyle">
+    <component :is="componentId" @onResourceLoad="handleResourceLoad"></component>
   </section>
 </template>
 <script>
-import { computed, reactive, defineAsyncComponent } from 'vue'
-export default {
+import { defineComponent, defineAsyncComponent, computed, onMounted, ref } from 'vue'
+export default defineComponent({
+  name: 'Screen',
   props: {
     screen: {
       type: String,
@@ -13,11 +14,35 @@ export default {
     }
   },
   setup(props) {
-    const componentName = reactive(props.screen)
-    const componentId = computed(() => defineAsyncComponent(() => import(`@/views/screens/${componentName}.vue`)))
+    const componentName = computed(() => props.screen.substring(0, 1).toUpperCase() + props.screen.substring(1))
+    const componentId = computed(() => defineAsyncComponent(() => import(`@/views/subpages/${componentName.value}.vue`)))
+    const h = ref(600)
+    const screenStyle = computed(() => {
+      return {
+        height: h.value + 'px'
+      }
+    })
+    onMounted(() => {
+      h.value = window.innerHeight
+      // console.log(`${componentName.value} is mounted`)
+    })
     return {
-      componentId
+      componentId,
+      screenStyle
+    }
+  },
+
+  methods: {
+    handleResourceLoad(params) {
+      // console.log('loading resource...')
+      this.$emit('onResourceLoad', params)
     }
   }
-}
+})
 </script>
+<style lang="scss">
+.screen-wrapper {
+  height: 100vh;
+  display: flex;
+}
+</style>
