4e0c456faaf5a4284ed41a472f0542303a1df45a
Sknight Qian <sknight127@sina.com>
2021-01-15 16:47:22 +0800

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -8,14 +8,51 @@ const devConfig = merge(baseConfig, {
   },
   configureWebpack: {
     devtool: 'source-map',
-    plugins: [
-      // insert CDN js
-      // new HtmlWebpackTagsPlugin({
-      //   append: false,
-      //   publicPath: false,
-      //   tags: ['http://somecdn.js']
-      // })
-    ]
+    plugins: [],
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        cacheGroups: {
+          dayjs: {
+            test: /[\\/]node_modules[\\/]dayjs[\\/]/,
+            name() {
+              return 'dayjs'
+            }
+          },
+          amap: {
+            test: /[\\/]node_modules[\\/]@amap[\\/]/,
+            name() {
+              return 'amap'
+            }
+          },
+          qrcode: {
+            test: /[\\/]node_modules[\\/]qrcode-module[\\/]/,
+            name() {
+              return 'qrcode'
+            }
+          },
+          // html2canvas: {
+          //   test: /[\\/]node_modules[\\/]html2canvas[\\/]/,
+          //   name() {
+          //     return 'html2canvas'
+          //   }
+          // },
+          swiper: {
+            test: /[\\/]node_modules[\\/]tiny-swiper[\\/]/,
+            name() {
+              return 'tiny-swiper'
+            }
+          },
+          vue: {
+            test: /[\\/]node_modules[\\/]@vue[\\/]/,
+            name() {
+              return 'vue'
+            }
+          }
+        }
+      }
+    }
   }
 })
 
